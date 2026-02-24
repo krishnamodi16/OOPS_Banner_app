5b866c5e6fb50765a1c08a8b8e908463c8c3f194
KRISHNA MODI <km4950@srmist.edu.in>
2026-02-24 11:18:06 +0530

    uc3 is displayed here

---
+++ uc3_printOOPS.java
@@ -0,0 +1,62 @@
+/**
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
+ * Uses String.join() to construct each line of the banner.
+ */
+
+public class uc3_printOOPS {
+
+    public static void main(String[] args) {
+
+        // Each println builds one row of the banner
+        // Letters: O  O  P  S
+
+        System.out.println(String.join(" ",
+                "  *  ",  // O
+                "  *  ",  // O
+                " *** ",  // P
+                " *** "   // S
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ", 
+                " *   * ", 
+                " *   * ", 
+                " **      "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " ***  ",
+                " ***  "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " **      ",
+                "      ** "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " **      ",
+                "      ** "
+        ));
+
+        System.out.println(String.join(" ",
+                "  *  ",
+                "  *  ",
+                " **     ",
+                " **    "
+        ));
+
+        System.out.println(String.join(" ",
+                "        ",
+                "        ",
+                "        ",
+                " ***  "
+        ));
+    }
+}
