0c83ea8aa431ba2dd8be81d5d22409758965d4c1
KRISHNA MODI <km4950@srmist.edu.in>
2026-03-11 08:44:52 +0530

    uc5 is displayed here

---
+++ uc5_printOOPS.java
@@ -0,0 +1,41 @@
+public class uc5_printOOPS {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner App - UC5");
+
+        // Inline array initialization using String.join()
+        String[] banner = {
+            String.join("", "*****", "  ", "*****", "  ", "*****", "  ", "*****"),
+            String.join("", "*   *", "  ", "*   *", "  ", "*   *", "  ", "*   *"),
+            String.join("", "*   *", "  ", "*   *", "  ", "*   *", "  ", "*   *"),
+            String.join("", "*****", "  ", "*****", "  ", "*****", "  ", "*****"),
+            String.join("", "*     ", "  ", "*   *", "  ", "*   *", "  ", "    *"),
+            String.join("", "*     ", "  ", "*   *", "  ", "*   *", "  ", "    *"),
+            String.join("", "*     ", "  ", "*****", "  ", "*****", "  ", "*****")
+        };
+
+        // Enhanced for loop to print each line
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}public class OOPSBannerUC5 {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner App - UC5");
+
+        // Inline array initialization using String.join()
+        String[] banner = {
+            String.join("", "*****", "  ", "*****", "  ", "*****", "  ", "*****"),
+            String.join("", "*   *", "  ", "*   *", "  ", "*   *", "  ", "*   *"),
+            String.join("", "*   *", "  ", "*   *", "  ", "*   *", "  ", "*   *"),
+            String.join("", "*****", "  ", "*****", "  ", "*****", "  ", "*****"),
+            String.join("", "*     ", "  ", "*   *", "  ", "*   *", "  ", "    *"),
+            String.join("", "*     ", "  ", "*   *", "  ", "*   *", "  ", "    *"),
+            String.join("", "*     ", "  ", "*****", "  ", "*****", "  ", "*****")
+        };
+
+        // Enhanced for loop to print each line
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
